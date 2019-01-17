d13e4efc6d729487d7395146ebd3ba8a5c7032e9
Giorgio Zanni <g.zanni@hotmail.com>
2019-01-17 00:08:19 -0300

    ADD: solution to hash problem 7

--- hashes/7.rb
+++ hashes/7.rb
@@ -8,7 +8,8 @@ inventario = {
 
 def print_menu
   puts (
-'1) Agregar un producto al inventario.
+'Ingrese una opción:
+1) Agregar un producto y su stock al inventario.
 2) Eliminar un producto del inventario.
 3) Actualizar información de un producto.
 4) Mostrar stock total.
@@ -17,24 +18,52 @@ def print_menu
 7) Salir.')
 end
 
-def verify_action?(n)
-  n > 0 || n < 8
+def add_or_update_product(str, inventory)
+  prod_array = str.split(',')
+  inventory[prod_array.first.to_sym] = prod_array.last.to_i
 end
 
-def add_product(str)
-  prod = str.split[0]
-  stock = str.split[1]
-  inventario[prod.to_sym] = stock
+def delete_product(product, inventory)
+  inventory.delete(product.to_sym)
 end
 
-def delete_prod(prod)
-  inventario.delete(prod.to_sym)
+def show_stock(inventory)
+  puts inventory.values.sum
 end
 
-print_menu
-action = gets.to_i
-while verify_action?(action)
-  puts 'La opción ingresada no es válida. Ingrese una de las siguientes:'
+def show_most_stock(inventory)
+  max = inventory.values.max
+  inventory.each { |prod, stock| puts "#{prod}: #{stock}" if stock == max }
+end
+
+def check_existence(product, inventory)
+  puts inventory.has_key?(product.to_sym) ? "El producto #{product} está en el inventario." : "El producto #{product} no está en inventario."
+end
+
+ask = true
+while ask
   print_menu
   action = gets.to_i
+  if action == 1
+    puts 'Ingrese el nombre del producto y su stock (separados por una coma):'
+    add_or_update_product(gets.chomp, inventario)
+  elsif action == 2
+    puts 'Ingrese el nombre del producto a eliminar:'
+    delete_product(gets.chomp, inventario)
+  elsif action == 3
+    puts 'Ingrese el nombre del producto y su stock (separados por una coma):'
+    add_or_update_product(gets.chomp, inventario)
+  elsif action == 4
+    show_stock(inventario)
+  elsif action == 5
+    show_most_stock(inventario)
+  elsif action == 6
+    puts 'Ingrese el nombre del producto a verificar:'
+    check_existence(gets.chomp, inventario)
+  elsif action == 7
+    ask = false
+    puts 'Adios!'
+  else
+    puts 'Opción inválida.'
+  end
 end
